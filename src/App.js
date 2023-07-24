cd9c91797aec3e3e5e181804e16ccf95fe83a279
Bishal Thapa <bishal.thap3@gmail.com>
2023-07-24 14:32:02 +0545

    Routing Implemented

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 
 import './App.css';
 import Navbar from './Components/Commons/Navbar';
+import About from './Components/Pages/About';
 import Home from "./Components/Pages/Home.js";
+import Recipes from './Components/Pages/Recipes';
+import { BrowserRouter as Router, Route ,Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
+    <Router>
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/Recipes' element={<Recipes />} />
+        <Route path='/About' element={<About />} />
+      </Routes>
+    </Router>
+      
     </>
   );
 }
--- src/Components/Commons/Navbar.js
+++ src/Components/Commons/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Menu, Input } from 'semantic-ui-react'
 import bookLogo from "../Images/bookLogo.jpg"
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
@@ -11,12 +12,18 @@ export default function Navbar() {
         </Menu.Item>
         <Menu.Item
           name='HOME'
+          as={Link} 
+          to="/"
         />
         <Menu.Item
-          name='ABOUT'         
+          name='ABOUT'
+          as={Link} 
+          to="/About"         
         />
         <Menu.Item
-          name='RECIPES'          
+          name='RECIPES'
+          as={Link} 
+          to="/Recipes"          
         />
         <Menu.Menu position='right'>
           <Menu.Item>
