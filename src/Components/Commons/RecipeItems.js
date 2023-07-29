2bb5af4a59ece77eb25cfde4c55d4c33988d6366
Bishal Thapa <bishal.thap3@gmail.com>
2023-07-29 12:43:21 +0545

    Recipe Items fetched and displayed from the API

---
+++ src/Components/Commons/RecipeItems.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Button, Card } from 'semantic-ui-react'
+
+export default function RecipeItems( {recipes} ) {
+  return (
+    <Card style={{height: 430}}> 
+      <img src={recipes.image_url} alt="RecipeImage" style={{height: 220 , padding: '5px 5px'}}/>
+      <Card.Content style={{marginTop: 20}}>
+        <Card.Header content = {recipes.title} />
+        <p>Rating: {Math.floor(recipes.social_rank * 100) / 100}% </p>
+        <Card.Description>
+          <h4>{recipes.publisher}</h4>  
+        </Card.Description>
+      </Card.Content>
+      <Card.Content>
+          <Button 
+            as = {Link}
+            to = '/'
+            content = 'Details'
+            color='green'
+            size='large'
+          />
+           <Button 
+            href = {recipes.source_url}
+            target = '_blank'
+            content = 'Source'
+            color='orange'
+            size='large'
+          />
+        </Card.Content>
+    </Card>
+  )
+}
