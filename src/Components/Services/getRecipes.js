3f5c996b81e3ae39913c1552dd0f14405038c0c5
Bishal Thapa <bishal.thap3@gmail.com>
2023-07-29 15:15:56 +0545

    API for Ingredients and details is added

--- src/Components/Services/getRecipes.js
+++ src/Components/Services/getRecipes.js
@@ -10,3 +10,14 @@ export  const  getRecipes = async (searchedQuery) => {
        return error.response
       }    
 }
+
+export  const  getRecipe = async (searchedQuery) => { 
+  try {
+    let response = await axios.get(`https://forkify-api.herokuapp.com/api/get?rId=${searchedQuery}`);
+    return response.data;
+  } 
+  catch (error) {
+   console.log("Error While fetching the Data", error.message);
+   return error.response
+  }    
+}
